88aafebb5442c3ab76d62c0257166e686075b83f
Daniel X Moore <yahivin@gmail.com>
2015-09-26 13:53:30 -0700

    :christmas_tree::honey_pot: Updated in browser at strd6.github.io/editor

--- docs/package.js
+++ docs/package.js
@@ -186,7 +186,7 @@
     },
     "pixie.cson": {
       "path": "pixie.cson",
-      "content": "version: \"0.3.1-pre.0\"\n",
+      "content": "version: \"0.3.1\"\n",
       "mode": "100644",
       "type": "blob"
     },
@@ -205,7 +205,7 @@
     },
     "pixie": {
       "path": "pixie",
-      "content": "module.exports = {\"version\":\"0.3.1-pre.0\"};",
+      "content": "module.exports = {\"version\":\"0.3.1\"};",
       "type": "blob"
     },
     "test/postmaster": {
@@ -217,7 +217,7 @@
   "progenitor": {
     "url": "http://www.danielx.net/editor/"
   },
-  "version": "0.3.1-pre.0",
+  "version": "0.3.1",
   "entryPoint": "main",
   "repository": {
     "branch": "master",
